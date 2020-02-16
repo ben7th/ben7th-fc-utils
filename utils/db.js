05fa12bc256be136cf93d0572cc0689215fee493
ben7th <ben7th@sina.com>
2020-02-17 03:34:31 +0800

    update mongoose

--- utils/db.js
+++ utils/db.js
@@ -1,27 +1,12 @@
 const mongoose = require('mongoose')
 const { DB_URI, DB_NAME } = process.env
 
-// 连接数据库并执行操作，操作完毕后关闭数据库连接
-const connectDB = async (callback = () => {}) => {
-  // connect 连接后必须调用 close 关闭连接，否则会导致进程死掉
-  await mongoose.connect(DB_URI, { 
-    useNewUrlParser: true,
-    dbName: DB_NAME
-  })
-
-  try {
-    await callback()
-  } catch (e) {
-    console.log(e)
-  } finally {
-    await mongoose.connection.close()
-  }
-}
-
 // 仅连接数据库
 const onlyConnect = async () => {
   await mongoose.connect(DB_URI, { 
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     dbName: DB_NAME
   })
 }
@@ -29,6 +14,21 @@ const onlyConnect = async () => {
 // 仅关闭数据库连接
 const onlyClose = async () => {
   await mongoose.connection.close()
+}
+
+// 连接数据库并执行操作，操作完毕后关闭数据库连接
+const connectDB = async (callback = () => {}) => {
+  // connect 连接后必须调用 close 关闭连接，否则会导致进程死掉
+  await onlyConnect()
+
+  try {
+    await callback()
+  } catch (e) {
+    console.log(e)
+    throw e
+  } finally {
+    await onlyClose()
+  }
 }
 
 module.exports = { connectDB, onlyConnect, onlyClose }
